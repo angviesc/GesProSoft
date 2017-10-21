bc60a720e01fce4ad14f734ca1eea9c9e3dd3157
Svare7 <4.20rck@gmail.com>
2017-10-21 12:47:24 -0500

    registros db

--- Base de datos/Cambiosdb.sql
+++ Base de datos/Cambiosdb.sql
@@ -1,2 +1,7 @@
-alter table personal add capital int not null
--> after nom;
+ALTER TABLE articulos ADD nombre VARCHAR(60) NOT NULL AFTER codigo;
+
+INSERT INTO empleados (id, nombre, apellidop, tel, calle, colonia, cod_pos, email)
+VALUES (1,'Administrador','Sistema','3 33 33 33', 'Calle numero 33', 'Colonia tal', '55000', 'email@email.com');
+
+INSERT INTO usuarios (usuario, password, id_empleado, tipo)
+VALUES ('Admin',md5('pass'), 1, 1);
